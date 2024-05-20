43bf1e1a7823ae2f1bdbfee74bc19405274d9203
heejung Kim <hjx66@naver.com>
2024-05-20 10:13:00 +0900

    hotfix: pinia-plugin-persistedstate 추가 & 헤더에 유저 정보 반응형으로 가져오기 (#8)

--- src/api/user.js
+++ src/api/user.js
@@ -1,27 +1,17 @@
 import axios from "axios";
 import { localAxios } from "@/api/http-commons";
 
-// const local = localAxios();
-
-// async function findById(user, success, fail){
-//     const token = localStorage.getItem("access-token");
-//     if (token) {
-//         config.headers.Authorization = `Bearer ${token}`;
-//       }
-//     console.log("findById header : " + localStorage.getItem("access-token"))
-//     await local.get(`/user/profile`).then(success).catch(fail);
-// }
 const userApi = axios.create({
-    baseURL: "http://localhost:80/api/v1/user",
-  });
-  
+  baseURL: "http://localhost:80/api/v1/user",
+});
+
 userApi.interceptors.request.use((config) => {
-    const token = localStorage.getItem("access-token");
-    console.log("token:" + token)
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-    return config;
-  });
+  const token = localStorage.getItem("access-token");
+  console.log("token:" + token);
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
 
 export default userApi;
--- src/views/user/LoginView.vue
+++ src/views/user/LoginView.vue
@@ -1,12 +1,7 @@
 <script setup>
-import { computed, ref } from "vue";
+import { ref } from "vue";
 import { useRouter } from "vue-router";
 import { useAuthStore } from "@/stores/auth";
-import axios, { HttpStatusCode } from "axios";
-import { jwtDecode } from "jwt-decode";
-// import {findById} from "@/api/user"
-import userApi from "@/api/user";
-const { VITE_IMAGE_BASE_URL } = import.meta.env;
 
 const authStore = useAuthStore();
 const router = useRouter();
@@ -16,7 +11,6 @@ const NAVER_URL =
 const GOOGLE_URL =
   "https://accounts.google.com/o/oauth2/auth/oauthchooseaccount?client_id=358553454680-ptcnidp5s8vufv27nr5l0l63cs9db48l.apps.googleusercontent.com&redirect_uri=http%3A%2F%2Flocalhost%3A80%2Fapi%2Fv1%2Fuser%2Flogin%2Fgoogle&response_type=code&scope=email%20profile&approval_prompt=force&access_type=offline&service=lso&o2v=1&ddm=0&flowName=GeneralOAuthFlow";
 
-// const user = computed(() => authStore.user);
 const user = ref(null);
 
 const login = async (url) => {
@@ -34,22 +28,7 @@ const logout = () => {
   user.value = null;
   router.push("/"); //홈으로 이동
 };
-const userInfo = authStore.userInfo;
 const imageUrl = ref(null);
-
-const getUserInfo = async () => {
-  const { data } = await userApi.get("/profile");
-  console.log(data);
-  userInfo.value = data;
-  if (userInfo.value) {
-    imageUrl.value = VITE_IMAGE_BASE_URL + userInfo.value.result.imageKey;
-    console.log("imageKey : " + imageUrl.value);
-  }
-};
-if (localStorage.getItem("access-token") != null) {
-  user.value = jwtDecode(localStorage.getItem("access-token"));
-  getUserInfo();
-}
 </script>
 
 <template>

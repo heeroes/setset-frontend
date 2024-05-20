1abf63ac9ea15796e913bb83aea950f107ebec19
hjx66 <hjx66@naver.com>
2024-05-20 10:10:39 +0900

    hotfix: pinia-plugin-persistedstate 추가 & 헤더에 유저 정보 반응형으로 가져오기

--- src/router/index.js
+++ src/router/index.js
@@ -24,6 +24,7 @@ const router = createRouter({
         console.log(token);
         const auth = useAuthStore();
         auth.setAccessToken(token);
+        auth.getUserInfo();
         next({ path: "/" });
       },
     },

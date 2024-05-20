a1811b995d91b5673db6b23fa3fdf0084d30d048
hjx66 <hjx66@naver.com>
2024-05-20 15:24:06 +0900

    feat: 마이페이지 그룹 관련 기능 구현 - 그룹 리스트 조회 - 그룹 생성 모달 - 그룹 구성원 초대 모달 - 그룹 탈퇴

---
+++ src/api/plan.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+import { localAxios } from "@/api/http-commons";
+
+const planApi = axios.create({
+  baseURL: "http://localhost:80/api/v1/plan",
+});
+
+planApi.interceptors.request.use((config) => {
+  const token = localStorage.getItem("access-token");
+  console.log("token:" + token);
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+
+export default planApi;

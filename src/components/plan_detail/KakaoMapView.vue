7803a87863116bdeaa3859395fa7a87ce001b113
hjx66 <hjx66@naver.com>
2024-05-22 11:20:24 +0900

    feat: 안전 정보 지도 표시 기능 구현 - 경찰서, 병원, 안전지킴이집 - 지도에 표시된 상태에서 다시 버튼을 누르면 지도에서 안보임

--- src/components/plan_detail/KakaoMapView.vue
+++ src/components/plan_detail/KakaoMapView.vue
@@ -7,10 +7,12 @@ import {
 } from "vue3-kakao-maps";
 
 import mapMarker0 from "@/assets/img/map/map_marker_0.png";
+import policeMarker from "@/assets/img/map/police_marker.png";
+
 import safeApi from "@/api/safe";
 
 // props 정의
-const props = defineProps(["planDetailArrays","planId"]);
+const props = defineProps(["planDetailArrays", "planId"]);
 
 // 마커 리스트 초기화
 const markerList = ref([]);
@@ -21,8 +23,6 @@ const lng = ref(126.573);
 
 // 기본 마커 리스트
 const defaultMarkers = ref([]);
-// 안전 정보 마커 리스트
-const safeMapMarkers = ref([]);
 
 // 일별 마커 정보 설정
 for (let i = 1; i < props.planDetailArrays.length; i++) {
@@ -64,35 +64,108 @@ if (props.planDetailArrays[0]) {
     });
   }
 }
+// 안전 정보 마커 리스트
+const hospitalInfo = ref([]);
+const policeInfo = ref([]);
+const guardHouseInfo = ref([]);
 
+const isHospital = ref(false);
+const isPoliceInfo = ref(false);
+const isGuardHouse = ref(false);
 //안전정보 마커 설정
 const getSafeMap = async (agencyType) => {
-  try{
-    const { response } = await safeApi.get("/map/" + props.planId,{
-      agencyType : agencyType
-    });
-    const safeLocationList = response.data.result;
-    for(const safe of safeLocationList){
-      safeMapMarkers.value.push({
+  try {
+    console.log("props.planId", props.planId);
+    if (agencyType == "hospital") {
+      isHospital.value = !isHospital.value;
+      if (!isHospital) {
+        hospitalInfo = ref([]);
+      } else if (hospitalInfo.value == []) {
+        preHospitalInfo();
+      }
+      return;
+    }
+
+    if (agencyType == "police") {
+      isPoliceInfo.value = !isPoliceInfo.value;
+      if (!isPoliceInfo) {
+        return;
+      } else {
+        fetchSafeMap(agencyType);
+      }
+    }
+
+    if (agencyType == "guardHouse") {
+      isGuardHouse.value = !isGuardHouse.value;
+      if (!isGuardHouse) {
+        return;
+      } else {
+        fetchSafeMap(agencyType);
+      }
+    }
+  } catch (error) {
+    console.log("안전정보 마커 에러", error);
+  }
+};
+
+const fetchSafeMap = async (agencyType) => {
+  const response = await safeApi.get(
+    `/map/${props.planId}?agencyType=${agencyType}&size=30`
+  );
+
+  console.log("response", response);
+
+  const safeLocationList = response.data.result;
+  console.log("safeLocationList:", safeLocationList);
+  const safeMapMarkers = ref(null);
+  for (const safe of safeLocationList) {
+    if (agencyType == "police") {
+      safeMapMarkers.value = policeInfo.value;
+    }
+    if (agencyType == "guardHouse") {
+      safeMapMarkers.value = guardHouseInfo.value;
+    }
+    safeMapMarkers.value.push({
       lat: safe.latitude,
       lng: safe.longitude,
       name: safe.name,
-      address:safe.address,
-      tel: safe.tel
-      })
-    }
-  }catch(error){
-    console.log("안전정보 마커 에러", error)
+      address: safe.address,
+      tel: safe.tel,
+      image: `/src/assets/img/map/${agencyType}_marker.png`,
+    });
   }
+};
 
-}
+const preHospitalInfo = async () => {
+  try {
+    const response = await safeApi.get(
+      `/map/${props.planId}?agencyType=hospital`
+    );
+    const safeLocationList = response.data.result;
+    for (const safe of safeLocationList) {
+      hospitalInfo.value.push({
+        lat: safe.latitude,
+        lng: safe.longitude,
+        name: safe.name,
+        address: safe.address,
+        tel: safe.tel,
+        image: `/src/assets/img/map/hospital_marker.png`,
+      });
+    }
+  } catch (error) {
+    console.log("안전정보 마커 에러", error);
+  }
+};
+
+// 페이지 로딩될 때 미리 데이터 받아 놓기
+preHospitalInfo();
 </script>
 
 <template>
   <div class="agencyType">
     <button @click="getSafeMap(`police`)">근처 경찰서</button>
-    <button>근처 응급센터</button>
-    <button>근처 안전지킴이집</button>
+    <button @click="getSafeMap(`hospital`)">근처 응급센터</button>
+    <button @click="getSafeMap(`guardHouse`)">근처 안전지킴이집</button>
   </div>
   <KakaoMap width="100%" height="100%" :lat="lat" :lng="lng" :level="8">
     <template v-for="(markers, index) in markerList" :key="index">
@@ -106,27 +179,47 @@ const getSafeMap = async (agencyType) => {
     </template>
 
     <template v-for="marker in defaultMarkers" :key="marker.lat + marker.lng">
+      <KakaoMapMarker :lat="marker.lat" :lng="marker.lng" :image="{ imageSrc:
+      `${mapMarker0}`, imageWidth: 48, imageHeight: 48, imageOption: {}, }" " />
+    </template>
+
+    <template v-for="marker in hospitalInfo" :key="marker.lat + marker.lng">
       <KakaoMapMarker
         :lat="marker.lat"
         :lng="marker.lng"
         :image="{
-          imageSrc: `${mapMarker0}`,
-          imageWidth: 48,
-          imageHeight: 48,
+          imageSrc: `${marker.image}`,
+          imageWidth: 40,
+          imageHeight: 40,
           imageOption: {},
         }"
+        v-if="isHospital"
       />
     </template>
-    <template v-for="marker in safeMap" :key="marker.lat + marker.lng">
+    <template v-for="marker in policeInfo" :key="marker.lat + marker.lng">
       <KakaoMapMarker
         :lat="marker.lat"
         :lng="marker.lng"
         :image="{
-          imageSrc: `https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/marker_red.png`,
-          imageWidth: 48,
-          imageHeight: 48,
+          imageSrc: `${marker.image}`,
+          imageWidth: 40,
+          imageHeight: 40,
           imageOption: {},
         }"
+        v-if="isPoliceInfo"
+      />
+    </template>
+    <template v-for="marker in guardHouseInfo" :key="marker.lat + marker.lng">
+      <KakaoMapMarker
+        :lat="marker.lat"
+        :lng="marker.lng"
+        :image="{
+          imageSrc: `${marker.image}`,
+          imageWidth: 40,
+          imageHeight: 40,
+          imageOption: {},
+        }"
+        v-if="isGuardHouse"
       />
     </template>
   </KakaoMap>

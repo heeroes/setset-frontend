2bc23a32d4dbaea2cc609da5c3cd80811801cbe2
hjx66 <hjx66@naver.com>
2024-05-23 14:28:57 +0900

    feat: 겸색 결과 페이지 디자인 완료 & favicon 추가

--- src/views/searchAttraction/SearchAttractionView.vue
+++ src/views/searchAttraction/SearchAttractionView.vue
@@ -8,10 +8,18 @@ const keyword = route.query.keyword;
 </script>
 
 <template>
-  <div>
-    <SearchBar />
+  <div class="container">
+    <SearchBar style="margin-top: 30px; margin-bottom: 20px; width: 100%" />
     <SearchResult :keyword="keyword" />
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.container {
+  margin-left: calc(580px);
+  margin-right: calc(580px);
+  width: calc(
+    100% - 1160px
+  ); /* 전체 너비에서 좌우 마진을 빼고 나머지를 너비로 설정 */
+}
+</style>

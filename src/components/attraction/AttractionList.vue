91e3bd1e763347a3ed65ec5b8dda52a851a3c800
heejung Kim <hjx66@naver.com>
2024-05-23 14:34:09 +0900

    feat: 겸색 결과 페이지 디자인 완료 & favicon 추가 (#30)

--- src/components/attraction/AttractionList.vue
+++ src/components/attraction/AttractionList.vue
@@ -94,6 +94,18 @@ const toggleDropdown = (index) => {
 const searchDetail = (id) => {
   router.push({ name: "AttractionDetail", params: { id } });
 };
+
+const truncateMessage = (message, maxLength) => {
+  // 메시지를 줄일 최대 길이
+  console.log(message);
+  if (message) {
+    if (message.length > maxLength) {
+      return message.substring(0, maxLength) + "...";
+    }
+  }
+  return message;
+};
+
 getPlanList();
 </script>
 
@@ -105,9 +117,29 @@ getPlanList();
         :key="attraction.id"
         class="attraction-item"
       >
+        <div @click="searchDetail(attraction.id)" class="attraction-info">
+          <div class="attraction-image">
+            <img
+              :src="attraction.image"
+              :alt="attraction.title"
+              v-if="attraction.image"
+            />
+            <div v-else class="no-image"></div>
+          </div>
+          <div class="attraction-details">
+            <h3 style="font-size: 24px">{{ attraction.title }}</h3>
+            <p style="color: #666; margin-bottom: 5px">
+              {{ attraction.addr }}
+            </p>
+            <p>{{ truncateMessage(attraction.overview, 65) }}</p>
+          </div>
+        </div>
         <!-- 드롭다운 버튼 추가 -->
         <div class="dropdown" @click="toggleDropdown(index)">
-          +
+          <button class="icon-btn add-btn">
+            <div class="add-icon"></div>
+            <div class="btn-txt">Add to Plan ▶</div>
+          </button>
           <div v-if="dropdownIndex === index" class="dropdown-content">
             <button
               v-for="plan in plans"
@@ -118,11 +150,6 @@ getPlanList();
               {{ plan.title }}
             </button>
           </div>
-        </div>
-        <div @click="searchDetail(attraction.id)">
-          <h3>{{ attraction.title }}</h3>
-          <img :src="attraction.image" :alt="attraction.title" />
-          <p>{{ attraction.addr }}</p>
         </div>
       </div>
     </div>
@@ -160,6 +187,12 @@ getPlanList();
 </template>
 
 <style scoped>
+.dropdown {
+  position: absolute; /* 드롭다운 버튼에 대해 position: absolute; 설정 */
+  top: -130px; /* 원하는 위치 조정 */
+  left: 650px;
+  cursor: pointer;
+}
 .attraction-section {
   margin-bottom: 40px;
 }
@@ -168,23 +201,251 @@ getPlanList();
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
+  display: flex;
+  flex-direction: column;
+  box-shadow: 2px 2px 10px #aaa;
 }
 
 .attraction-item {
-  width: 300px;
-  border: 1px solid #ccc;
+  width: 100%;
+  height: 170px;
+  border-bottom: 1px solid #ccc;
   border-radius: 4px;
-  margin: 10px;
+  /* margin: 10px; */
   padding: 20px;
-  box-shadow: 2px 2px 12px #aaa;
+  position: relative; /* 부모 요소에 대해 position: relative; 설정 */
 }
-
+.attraction-image {
+  width: 30%;
+  height: auto;
+  max-height: 120px;
+  margin-left: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 .attraction-item img {
-  width: 80%;
-  height: 60%;
+  width: 100%;
+  height: auto;
+  max-height: 120px;
   border-radius: 4px;
 }
 
+.dropdown-content {
+  position: absolute;
+  top: 50%;
+  left: 100%;
+  background-color: white;
+  border: 1px solid #ccc;
+  z-index: 1000;
+}
+
+.dropdown-content button {
+  display: block;
+  width: 120px;
+  padding: 8px 12px;
+  text-align: left;
+  background: none;
+  border: none;
+  cursor: pointer;
+}
+
+.dropdown-content button:hover {
+  background-color: #f1f1f1;
+}
+.attraction-info {
+  display: flex;
+  align-items: center;
+}
+
+.attraction-details {
+  flex: 1;
+  margin-left: 30px;
+}
+
+.attraction-details h3 {
+  margin: 0 0 5px 0;
+}
+
+.attraction-details p {
+  margin: 0;
+}
+
+/* 계획 추가 버튼 */
+.icon-btn {
+  width: 50px;
+  height: 50px;
+  border: 1px solid #cdcdcd;
+  background: white;
+  border-radius: 25px;
+  overflow: hidden;
+  position: relative;
+  transition: width 0.2s ease-in-out;
+  font-weight: 500;
+  font-family: inherit;
+}
+
+.add-btn:hover {
+  width: 120px;
+}
+
+.add-btn::before,
+.add-btn::after {
+  transition: width 0.2s ease-in-out, border-radius 0.2s ease-in-out;
+  content: "";
+  position: absolute;
+  height: 4px;
+  width: 10px;
+  top: calc(50% - 2px);
+  background: seagreen;
+}
+
+.add-btn::after {
+  right: 14px;
+  overflow: hidden;
+  border-top-right-radius: 2px;
+  border-bottom-right-radius: 2px;
+}
+
+.add-btn::before {
+  left: 14px;
+  border-top-left-radius: 2px;
+  border-bottom-left-radius: 2px;
+}
+
+.icon-btn:focus {
+  outline: none;
+}
+
+.btn-txt {
+  opacity: 0;
+  transition: opacity 0.2s;
+  font-size: 11px;
+}
+
+.add-btn:hover::before,
+.add-btn:hover::after {
+  width: 4px;
+  border-radius: 2px;
+}
+
+.add-btn:hover .btn-txt {
+  opacity: 1;
+}
+
+.add-icon::after,
+.add-icon::before {
+  transition: all 0.2s ease-in-out;
+  content: "";
+  position: absolute;
+  height: 20px;
+  width: 2px;
+  top: calc(50% - 10px);
+  background: seagreen;
+  overflow: hidden;
+}
+
+.add-icon::before {
+  left: 22px;
+  border-top-left-radius: 2px;
+  border-bottom-left-radius: 2px;
+}
+
+.add-icon::after {
+  right: 22px;
+  border-top-right-radius: 2px;
+  border-bottom-right-radius: 2px;
+}
+
+.add-btn:hover .add-icon::before {
+  left: 15px;
+  height: 4px;
+  top: calc(50% - 2px);
+}
+
+.add-btn:hover .add-icon::after {
+  right: 15px;
+  height: 4px;
+  top: calc(50% - 2px);
+}
+
+/* no image */
+.no-image {
+  width: 64px;
+  height: 64px;
+  position: relative;
+  background: #fff;
+  border-radius: 4px;
+  overflow: hidden;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.no-image:before {
+  content: "";
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  width: 40px;
+  height: 40px;
+  transform: rotate(45deg) translate(30%, 40%);
+  background: #ff9371;
+  box-shadow: 32px -34px 0 5px #ff3d00;
+  animation: slide 2s infinite ease-in-out alternate;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.no-image:after {
+  content: "";
+  position: absolute;
+  left: 10px;
+  top: 10px;
+  width: 16px;
+  height: 16px;
+  border-radius: 50%;
+  background: #ff3d00;
+  transform: rotate(0deg);
+  transform-origin: 35px 145px;
+  animation: rotate 2s infinite ease-in-out;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+@keyframes slide {
+  0%,
+  100% {
+    bottom: -35px;
+  }
+
+  25%,
+  75% {
+    bottom: -2px;
+  }
+
+  20%,
+  80% {
+    bottom: 2px;
+  }
+}
+
+@keyframes rotate {
+  0% {
+    transform: rotate(-15deg);
+  }
+
+  25%,
+  75% {
+    transform: rotate(0deg);
+  }
+
+  100% {
+    transform: rotate(25deg);
+  }
+}
 .pagination {
   display: flex;
   justify-content: center;
--- src/components/attraction/SearchResult.vue
+++ src/components/attraction/SearchResult.vue
@@ -138,12 +138,22 @@ const toggleDropdown = (index) => {
 const searchDetail = (id) => {
   router.push({ name: "AttractionDetail", params: { id } });
 };
+
+const truncateMessage = (message, maxLength) => {
+  // 메시지를 줄일 최대 길이
+  console.log(message);
+  if (message) {
+    if (message.length > maxLength) {
+      return message.substring(0, maxLength) + "...";
+    }
+  }
+  return message;
+};
 getPlanList();
 </script>
 
 <template>
   <div>
-    <h1>검색 결과 리스트</h1>
     <div class="tabs">
       <button
         v-for="type in [{ value: 'all', name: '전체' }, ...attractionTypes]"
@@ -154,22 +164,56 @@ getPlanList();
         {{ type.name }}
       </button>
     </div>
+    <div class="search-keyword">
+      <span style="font-size: 26px; color: #e74c3c">{{ keyword }}</span>
+      <span style="font-size: 20px">를 검색한 결과입니다.</span>
+    </div>
+
     <div v-if="activeType === 'all'">
       <div
         v-for="type in attractionTypes"
         :key="type.value"
         class="attraction-section"
       >
-        <h2>{{ type.name }}</h2>
+        <h2
+          style="
+            font-size: 25px;
+            margin-bottom: 10px;
+            margin-top: 10px;
+            padding-left: 10px;
+          "
+        >
+          🚩 {{ type.name }}
+        </h2>
         <div v-if="attractions[type.value] != []" class="attraction-list">
           <div
             v-for="(attraction, index) in attractions[type.value]"
             :key="attraction.id"
             class="attraction-item"
           >
+            <div @click="searchDetail(attraction.id)" class="attraction-info">
+              <div class="attraction-image">
+                <img
+                  :src="attraction.image"
+                  :alt="attraction.title"
+                  v-if="attraction.image"
+                />
+                <div v-else class="no-image"></div>
+              </div>
+              <div class="attraction-details">
+                <h3 style="font-size: 24px">{{ attraction.title }}</h3>
+                <p style="color: #666; margin-bottom: 5px">
+                  {{ attraction.addr }}
+                </p>
+                <p>{{ truncateMessage(attraction.overview, 65) }}</p>
+              </div>
+            </div>
             <!-- 드롭다운 버튼 추가 -->
             <div class="dropdown" @click="toggleDropdown(index)">
-              +
+              <button class="icon-btn add-btn">
+                <div class="add-icon"></div>
+                <div class="btn-txt">Add to Plan ▶</div>
+              </button>
               <div v-if="dropdownIndex === index" class="dropdown-content">
                 <button
                   v-for="plan in plans"
@@ -181,13 +225,10 @@ getPlanList();
                 </button>
               </div>
             </div>
-            <div @click="searchDetail(attraction.id)">
-              <h3>{{ attraction.title }}</h3>
-              <img :src="attraction.image" :alt="attraction.title" />
-              <p>{{ attraction.addr }}</p>
-            </div>
           </div>
-          <button @click="goToType(type.value)">+ 더보기</button>
+          <button @click="goToType(type.value)" style="height: 80px">
+            + 더보기
+          </button>
         </div>
         <div v-else>
           <p>검색 결과가 없습니다.</p>
@@ -212,17 +253,29 @@ getPlanList();
 .tabs {
   display: flex;
   margin-bottom: 20px;
+  background-color: #fff;
+  box-shadow: 0 0 1px 0 rgba(24, 94, 224, 0.15),
+    0 6px 12px 0 rgba(24, 94, 224, 0.15);
+  padding: 0.75rem;
+  border-radius: 99px;
+  position: relative;
 }
 
 .tabs button {
   padding: 10px 20px;
   border: none;
-  background-color: #f0f0f0;
   cursor: pointer;
 }
 
 .tabs button.active {
-  background-color: #ccc;
+  background-color: #abd373;
+  border-radius: 99px;
+  color: white;
+}
+.search-keyword {
+  padding-left: 20px;
+  margin-bottom: 30px;
+  margin-top: 20px;
 }
 
 .attraction-section {
@@ -233,32 +286,49 @@ getPlanList();
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
+  display: flex;
+  flex-direction: column;
+  box-shadow: 2px 2px 10px #aaa;
 }
 
 .attraction-item {
-  width: 300px;
-  border: 1px solid #ccc;
-  border-radius: 4px;
-  margin: 10px;
-  padding: 20px;
-  box-shadow: 2px 2px 12px #aaa;
-}
+  width: 100%;
+  height: 170px;
+  border-top: 1px solid #ccc;
+  border-bottom: 1px solid #ccc;
 
+  border-radius: 4px;
+  /* margin: 10px; */
+  padding: 20px;
+  position: relative; /* 부모 요소에 대해 position: relative; 설정 */
+}
+.attraction-image {
+  width: 30%;
+  height: auto;
+  max-height: 120px;
+  margin-left: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
 .attraction-item img {
-  width: 80%;
-  height: 60%;
+  width: 100%;
+  height: auto;
+  max-height: 120px;
   border-radius: 4px;
 }
 
 .dropdown {
-  position: relative;
+  position: absolute; /* 드롭다운 버튼에 대해 position: absolute; 설정 */
+  top: 10px; /* 원하는 위치 조정 */
+  right: 10px; /* 원하는 위치 조정 */
   cursor: pointer;
 }
 
 .dropdown-content {
   position: absolute;
-  top: 100%;
-  right: 0;
+  top: 50%;
+  left: 100%;
   background-color: white;
   border: 1px solid #ccc;
   z-index: 1000;
@@ -266,7 +336,7 @@ getPlanList();
 
 .dropdown-content button {
   display: block;
-  width: 100px;
+  width: 120px;
   padding: 8px 12px;
   text-align: left;
   background: none;
@@ -276,5 +346,198 @@ getPlanList();
 
 .dropdown-content button:hover {
   background-color: #f1f1f1;
+}
+.attraction-info {
+  display: flex;
+  align-items: center;
+}
+
+.attraction-details {
+  flex: 1;
+  margin-left: 30px;
+}
+
+.attraction-details h3 {
+  margin: 0 0 5px 0;
+}
+
+.attraction-details p {
+  margin: 0;
+}
+
+/* 계획 추가 버튼 */
+.icon-btn {
+  width: 50px;
+  height: 50px;
+  border: 1px solid #cdcdcd;
+  background: white;
+  border-radius: 25px;
+  overflow: hidden;
+  position: relative;
+  transition: width 0.2s ease-in-out;
+  font-weight: 500;
+  font-family: inherit;
+}
+
+.add-btn:hover {
+  width: 120px;
+}
+
+.add-btn::before,
+.add-btn::after {
+  transition: width 0.2s ease-in-out, border-radius 0.2s ease-in-out;
+  content: "";
+  position: absolute;
+  height: 4px;
+  width: 10px;
+  top: calc(50% - 2px);
+  background: seagreen;
+}
+
+.add-btn::after {
+  right: 14px;
+  overflow: hidden;
+  border-top-right-radius: 2px;
+  border-bottom-right-radius: 2px;
+}
+
+.add-btn::before {
+  left: 14px;
+  border-top-left-radius: 2px;
+  border-bottom-left-radius: 2px;
+}
+
+.icon-btn:focus {
+  outline: none;
+}
+
+.btn-txt {
+  opacity: 0;
+  transition: opacity 0.2s;
+  font-size: 11px;
+}
+
+.add-btn:hover::before,
+.add-btn:hover::after {
+  width: 4px;
+  border-radius: 2px;
+}
+
+.add-btn:hover .btn-txt {
+  opacity: 1;
+}
+
+.add-icon::after,
+.add-icon::before {
+  transition: all 0.2s ease-in-out;
+  content: "";
+  position: absolute;
+  height: 20px;
+  width: 2px;
+  top: calc(50% - 10px);
+  background: seagreen;
+  overflow: hidden;
+}
+
+.add-icon::before {
+  left: 22px;
+  border-top-left-radius: 2px;
+  border-bottom-left-radius: 2px;
+}
+
+.add-icon::after {
+  right: 22px;
+  border-top-right-radius: 2px;
+  border-bottom-right-radius: 2px;
+}
+
+.add-btn:hover .add-icon::before {
+  left: 15px;
+  height: 4px;
+  top: calc(50% - 2px);
+}
+
+.add-btn:hover .add-icon::after {
+  right: 15px;
+  height: 4px;
+  top: calc(50% - 2px);
+}
+
+/* no image */
+.no-image {
+  width: 64px;
+  height: 64px;
+  position: relative;
+  background: #fff;
+  border-radius: 4px;
+  overflow: hidden;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.no-image:before {
+  content: "";
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  width: 40px;
+  height: 40px;
+  transform: rotate(45deg) translate(30%, 40%);
+  background: #ff9371;
+  box-shadow: 32px -34px 0 5px #ff3d00;
+  animation: slide 2s infinite ease-in-out alternate;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.no-image:after {
+  content: "";
+  position: absolute;
+  left: 10px;
+  top: 10px;
+  width: 16px;
+  height: 16px;
+  border-radius: 50%;
+  background: #ff3d00;
+  transform: rotate(0deg);
+  transform-origin: 35px 145px;
+  animation: rotate 2s infinite ease-in-out;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+@keyframes slide {
+  0%,
+  100% {
+    bottom: -35px;
+  }
+
+  25%,
+  75% {
+    bottom: -2px;
+  }
+
+  20%,
+  80% {
+    bottom: 2px;
+  }
+}
+
+@keyframes rotate {
+  0% {
+    transform: rotate(-15deg);
+  }
+
+  25%,
+  75% {
+    transform: rotate(0deg);
+  }
+
+  100% {
+    transform: rotate(25deg);
+  }
 }
 </style>
